c5962611a81d4ae983ea850ba631194c6750635d
shuvo1997 <51527801+shuvo1997@users.noreply.github.com>
2020-09-02 23:52:42 +0600

    Added options & showing correct answer.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:myquizapp/models/quiz_model.dart';
@@ -27,6 +28,97 @@ class _QuizPageState extends State<QuizPage> {
   int questionNumber = 0;
   Future<Quiz> futureQuiz;
   HtmlEncoding encoding = new HtmlEncoding();
+  bool _isVisible = true;
+
+  // TO show the correct answer of a question
+  Widget showingAnswer(String correctAns) {
+    return Visibility(
+      visible: !_isVisible,
+      child: Column(
+        children: [
+          Text(
+            'The correct answer is',
+            style: TextStyle(fontSize: 20),
+          ),
+          FlatButton(
+            onPressed: () {},
+            child: Text(
+              '$correctAns',
+              style: TextStyle(fontSize: 20),
+            ),
+            color: Colors.green,
+          ),
+          FlatButton(
+            onPressed: () {
+              setState(() {
+                questionNumber++;
+                _isVisible = true;
+              });
+            },
+            child: Text(
+              'Next Question',
+              style: TextStyle(fontSize: 20),
+            ),
+            color: Colors.red[900],
+          )
+        ],
+      ),
+    );
+  }
+
+  //To build each option button
+  Widget optionButton(
+      int number, List<String> options, List<int> list, String correctAns) {
+    return Visibility(
+      visible: _isVisible,
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            _isVisible = false;
+          });
+        },
+        child: Text(
+          options[list[number]],
+          style: TextStyle(fontSize: 20, letterSpacing: 2),
+        ),
+        color: Colors.blue,
+      ),
+    );
+  }
+
+  //To randomize the options and build the options.
+  Widget optionBuilder(String correct_ans, List<String> incorrect_ans) {
+    if (correct_ans != null && incorrect_ans != null) {
+      List<String> options = [];
+      options.add(correct_ans);
+      options.addAll(incorrect_ans);
+      Random random = new Random();
+      List<int> list = [];
+      int max = 4;
+      while (list.length < 4) {
+        int random_num = random.nextInt(max);
+        if (!list.contains(random_num)) {
+          list.add(random_num);
+        }
+      }
+      return Column(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              optionButton(0, options, list, correct_ans),
+              optionButton(1, options, list, correct_ans),
+              optionButton(2, options, list, correct_ans),
+              optionButton(3, options, list, correct_ans),
+              showingAnswer(correct_ans),
+            ],
+          ),
+        ],
+      );
+    } else {
+      return Text('No Button');
+    }
+  }
 
   @override
   void initState() {
@@ -46,37 +138,33 @@ class _QuizPageState extends State<QuizPage> {
         child: Center(
           child: Padding(
             padding: EdgeInsets.all(8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FutureBuilder<Quiz>(
-                  future: futureQuiz,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      if (snapshot.hasError) {
-                        return Text('Error');
-                      }
-                      String text = encoding.htmlDecoder(
-                          snapshot.data.results[questionNumber].question);
-                      return Text(
+            child: FutureBuilder<Quiz>(
+              future: futureQuiz,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return Text('Error');
+                  }
+                  String text = encoding.htmlDecoder(
+                      snapshot.data.results[questionNumber].question);
+                  String correctAns =
+                      snapshot.data.results[questionNumber].correct_answer;
+                  List<String> incorrectAns =
+                      snapshot.data.results[questionNumber].incorrect_answers;
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
                         '$text',
-                        style: TextStyle(fontSize: 20),
-                      );
-                    } else {
-                      return CircularProgressIndicator();
-                    }
-                  },
-                ),
-                FlatButton(
-                  onPressed: () {
-                    setState(() {
-                      questionNumber++;
-                    });
-                  },
-                  child: Text('Next Question'),
-                  color: Colors.blue[300],
-                )
-              ],
+                        style: TextStyle(fontSize: 25, letterSpacing: 2),
+                      ),
+                      optionBuilder(correctAns, incorrectAns),
+                    ],
+                  );
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
             ),
           ),
         ),
--- lib/services/quiz_services.dart
+++ lib/services/quiz_services.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'package:myquizapp/models/quiz_model.dart';
 
-String url = 'https://opentdb.com/api.php?amount=10';
+String url = 'https://opentdb.com/api.php?amount=10&type=multiple';
 
 Future<Quiz> getQuiz() async {
   final response = await http.get('$url');
